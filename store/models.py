4fb7d79016205d07e3a02679af31f9164a561ddb
Nbdyleto <nobodyleto2004@gmail.com>
2023-02-10 18:21:06 -0300

    creating models

--- store/models.py
+++ store/models.py
@@ -38,3 +38,10 @@ class ShippingAddress(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, null = True)
     adress = models.CharField(max_length=200, null=False)
+    city = models.CharField(max_length=200, null=False)
+    state = models.CharField(max_length=200, null=False)
+    zipcode = models.CharField(max_length=200, null=False)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.adress
