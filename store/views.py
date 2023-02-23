513d657f085f46edd1f2ad56a1e86170b3048800
Nbdyleto <nobodyleto2004@gmail.com>
2023-02-23 18:04:32 -0300

    make payment button and manipulate html based on shipping and user login status

--- store/views.py
+++ store/views.py
@@ -45,7 +45,7 @@ def checkout(request):
         cartItems = order.get_cart_items
     else:
         items = []
-        order = {'get_cart_total':0, 'get_cart_items':0, 'shipping':False   }
+        order = {'get_cart_total':0, 'get_cart_items':0, 'shipping':False}
         cartItems = order['get_cart_items']
     context = {'items':items, 'order':order, 'cartItems':cartItems}
     return render(request, 'store/checkout.html', context)
